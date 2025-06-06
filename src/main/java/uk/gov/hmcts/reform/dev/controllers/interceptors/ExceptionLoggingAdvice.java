577c22921ad83dbed2fac43bbf86d98d351494d0
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2025-06-06 16:59:51 +0100

    Update ExceptionLoggingAdvice.java

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/interceptors/ExceptionLoggingAdvice.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/interceptors/ExceptionLoggingAdvice.java
@@ -24,10 +24,7 @@ public class ExceptionLoggingAdvice {
     private final RequestAuditLogService requestAuditLogService;
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> logAllExceptions(Exception ex, WebRequest webRequest) throws Exception {
-        HttpServletRequest request =
-            (HttpServletRequest) webRequest.resolveReference(WebRequest.REFERENCE_REQUEST);
-
+    public void logAllExceptions(Exception ex, HttpServletRequest request) throws Exception {
         log.error(REQUEST_ERROR_FORMAT, request.getMethod(), request.getRequestURI(), ex);
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
